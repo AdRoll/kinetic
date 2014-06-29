eb2eef26cd0f82b986add9bc793284596abdac96
dialtone <dialtone@adroll.com>
2014-06-29 02:00:06 -0700

    initial app skeleton

---
+++ src/kinetic.erl
@@ -0,0 +1,27 @@
+-module(kinetic).
+-behaviour(application).
+
+
+-export([start/0, stop/0]).
+-export([start/2, stop/1]).
+
+
+-spec start() -> ok | {error, any()}.
+start() ->
+    application:start(lhttpc).
+
+
+-spec stop() -> ok | {error, any()}.
+stop() ->
+    application:stop(lhttpc).
+
+
+-spec start(normal | {takeover, node()} | {failover, node()}, any()) ->
+    {ok, pid()}.
+start(_, Opts) ->
+    kinetic_sup:start_link().
+
+-spec stop(any()) -> ok.
+stop(_) ->
+    ok.
+
---
+++ src/kinetic_sup.erl
@@ -0,0 +1,30 @@
+-module(kinetic_sup).
+-behaviour(supervisor).
+
+-export([start_link/0, start_link/1]).
+-export([init/1]).
+
+-type child() :: {atom(), {atom(), atom(), list(any)},
+    atom(), integer(), atom(), list(atom())}.
+
+-spec start_link() -> {ok, pid()} | {error, atom()}.
+start_link() ->
+    supervisor:start_link({local, ?MODULE}, ?MODULE, []).
+
+start_link(Args) ->
+    supervisor:start_link({local, ?MODULE}, ?MODULE, Args).
+
+-spec init(any()) -> {ok, {{atom(), integer(), integer()}, [child()]}}.
+init(_Opts) ->
+    init_ets(),
+    {ok, {{simple_one_for_one, 10, 1}, [
+        {kinetic_logger,
+         {kinetic_logger, start_link, []},
+         transient, 10000, worker, [kinetic_logger]}
+    ]}}.
+
+init_ets() ->
+    ets:new(kinetic, [named_table, set, public, {read_concurrency, true}]).
+
+
+
