e21b7485d31e500a75e8523cef7d124a3cb4d619
Pablo Lopez <pablo.lopez@nextroll.com>
2025-08-05 17:04:07 +0200

    fix bug

--- src/kinetic_utils.erl
+++ src/kinetic_utils.erl
@@ -3,13 +3,13 @@
 -export([pool_name/1, regions/0, region/1, endpoint/1, decode/1, encode/1]).
 
 pool_name("us-east-1" ++ _R) ->
-    'dinerl_pool_us-east-1';
+    'kinetic_pool_us-east-1';
 pool_name("us-west-2" ++ _R) ->
-    'dinerl_pool_us-west-2';
+    'kinetic_pool_us-west-2';
 pool_name("ap-southeast-1" ++ _R) ->
-    'dinerl_pool_ap-southeast-1';
+    'kinetic_pool_ap-southeast-1';
 pool_name("eu-west-1" ++ _R) ->
-    'dinerl_pool_eu-west-1'.
+    'kinetic_pool_eu-west-1'.
 
 regions() ->
     ["us-east-1", "us-west-2", "ap-southeast-1", "eu-west-1"].
