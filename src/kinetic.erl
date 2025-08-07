98f20ad29b2e8df9cfa194b9cc4a63c4aa2431ba
Pablo Lopez <pablo.lopez@nextroll.com>
2025-08-07 13:43:57 +0200

    remove max_inactive conf as it is not needed

--- src/kinetic.erl
+++ src/kinetic.erl
@@ -318,7 +318,6 @@ start_pool() ->
                                            [{host, Endpoint},
                                             {port, 443},
                                             {pool_size, PoolSize},
-                                            {max_inactive, 200},
                                             {gun_opts, GunOpts}])
                   end,
                   kinetic_utils:regions()).
